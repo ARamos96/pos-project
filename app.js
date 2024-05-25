749ea698ccd43f5d39cf8937cfe7c1d8b1473fbf
Arnau Ramos <arnauramos21@gmail.com>
2024-05-25 15:21:53 +0200

    created product.route

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use("/auth", authRouter);
 const adminRouter = require("./routes/admin.routes")
 app.use("/admin", adminRouter);
 
+const productRouter = require('./routes/product.routes')
+app.use("/products", productRouter);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
